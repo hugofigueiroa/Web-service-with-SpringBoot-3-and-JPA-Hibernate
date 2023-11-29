4b09546de227b1bd55fad7acaceda13adf051e9b
Hugo Bezerra <hugobezerradev@gmail.com>
2023-11-28 21:06:19 -0300

    Web service with SpringBoot 3 and JPA/ Hibernate

---
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -0,0 +1,65 @@
+package com.educandoweb.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.service.exceptions.DataBaseException;
+import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository ;
+	
+	public List<User> findAll() {
+		return repository.findAll(); 
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
+	}
+	
+	public User insertUser(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void deleteUser(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+	
+	public User updateUser(Long id , User obj) {
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setEmail(obj.getEmail());
+		entity.setName(obj.getName());
+		entity.setPhone(obj.getPhone());
+	}
+}
